812588f6769c0abdab2fae010116136a8ea1f7d9
roram <ces.ram.franco@gmail.com>
2021-02-13 18:54:29 -0300

    Implementacion de controladores

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+const UserBase = require('../models/UserBase')
+const UserProvider = require('../models/UserProvider')
+
+const userController = {
+   addUserProvider: (req, res) =>{
+      // Desestructuro la req del front-end
+      const {firstName, lastName, urlPic, email, phone, password, country,
+      website, valoration, review, rol, idProfession} = req.body
+
+      const userBase = new UserBase ({
+         firstName, lastName, urlPic, email, phone, password, country
+      })
+      // Guardo en la base de datos el usuario base y luego lo voy a popular en el idUserBase para tener el resto de los datos
+      var newUserBase = userBase.save()
+
+      const idUserBase = newUserBase._id
+
+      const userProvider = new UserProvider({
+         idUserBase, website, valoration, review, rol, idProfession
+      })
+      userProvider.save()
+      .then(async newUserProvider =>{
+         // Populo el UserBase dentro del UserProvider para obtener el usuario mas sus datos
+         const populateUserProvider = await UserProvider.findById(newUserProvider._id).populate('idUserBase')
+         res.json({success:true, respuesta:populateUserProvider})
+      })
+      .catch(error => {return res.json({success:false, error})})
+   },
+   addUserCustomer:(req, res) =>{
+      const {firstName, lastName, urlPic, email, phone, password, country} = req.body
+   
+         const userBase = new UserBase ({
+            firstName, lastName, urlPic, email, phone, password, country
+         })
+         // Guardo en la base de datos el usuario base y luego lo voy a popular en el idUserBase para tener el resto de los datos
+         var newUserBase = userBase.save()
+   
+         const idUserBase = newUserBase._id
+   
+         const userCustomer = new UserCustomer({
+            idUserBase
+         })
+         userCustomer.save()
+         .then(async newUserCustomer =>{
+            // Populo el UserBase dentro del UserProvider para obtener el usuario mas sus datos
+            const populateUserCustomer = await UserCustomer.findById(newUserCustomer._id).populate('idUserBase')
+            res.json({success:true, respuesta:populateUserCustomer})
+         })
+         .catch(error => {return res.json({success:false, error})})
+   }
+}
+
+module.exports = userController
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+require('dotenv').config()
+
+const router = require('./routes')
+require('./config/database')
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', router)
+
+app.listen(4000, ()=> console.log('app listening in the port 4000'))
