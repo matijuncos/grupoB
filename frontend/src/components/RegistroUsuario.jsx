b75751336e41aee9e5552df3540dc287733ca3ab
Lucascachazo <luki_89@hotmail.com>
2021-02-13 20:52:45 -0300

    agregue componentes de registro

---
+++ frontend/src/components/RegistroUsuario.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import {useState} from 'react'
+import GoogleLogin from 'react-google-login'
+
+
+ function RegistroUsuario() {
+    const [nuevoUsuario , setNuevoUsuario]  = useState({})
+// Funcion para ler input
+    const leerInput = e => {
+        const campo = e.target.name
+        const valor = e.target.value
+        
+        setNuevoUsuario({
+            ...nuevoUsuario ,
+            [campo] : valor
+        })
+    }
+
+
+//Funcion para enviar formulario 
+
+    console.log(nuevoUsuario)
+      const validarUsuario  = async e =>{
+       
+        alert ('Me hiciste Click')
+        e.preventDefault() 
+       //Conexion a redux para enviar el objeto a endpoint y guardarlo con el {completo}
+    
+      
+    }
+
+
+//Respuesta de Google
+    const responseGoogle = async (response) => {
+    console.log(response)
+    
+    }
+
+    
+    
+    
+    
+    return (
+       <form>
+
+            <div>
+                <input name = 'Nombre' type='text' placeholder= 'Nombre' onChange ={leerInput}/>
+            </div>
+            <div>
+                <input  name = 'Apellido' type='' placeholder= 'Apellido' onChange ={leerInput}/>
+            </div>
+            <div>
+                <input name = 'Email' type='text' placeholder= 'Email' onChange ={leerInput}/>
+            </div>
+            <div>
+                <input name = 'Telefono' type='text' placeholder= 'Telefono' onChange ={leerInput}/>
+            </div>
+            <div>
+                <input name = 'Password' type='password' placeholder= 'Contraseña' onChange ={leerInput}/>
+            </div>
+            <div>
+                <button onClick = {validarUsuario}>Enviar Registro</button>
+            </div>
+            <div>
+            <GoogleLogin className="google"
+                    clientId="56670268622-ujtfv11jtt2esb9qe4cgo4drut70tgu4.apps.googleusercontent.com"
+                    buttonText="Crear cuenta con Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </div>
+
+        </form>
+    )
+}
+export default RegistroUsuario
