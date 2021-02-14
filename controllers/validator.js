c380eab65b16c526fad1eaaf1c3069744e715488
lucia <pereyra.lucia@gmail.com>
2021-02-14 20:18:35 -0300

    pull rama mati

---
+++ controllers/validator.js
@@ -0,0 +1,30 @@
+const Joi = require("joi")
+
+const validator = {
+// valida los campos esenciales (userBase)
+    validNewUser: (req, res, next) => {
+        const schema = Joi.object({
+            firstName: Joi.string().trim().required().min(2).max(10),
+            lastName: Joi.string().trim().required().min(2).max(10),
+            urlPic: Joi.string().uri().required(),
+            email: Joi.string().trim().required().email({minDomainSegments: 1, tlds: {allow: false}}),
+            phone:Joi.string().length(11).regex(/^[0-9]{11}$/).required(),
+            password: Joi.string().trim().required().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,8}$/), // PASS: LMAYUSC, LMINUSC,N0-9,3,8CARAC
+            country: Joi.string().required()
+        })
+
+        const validation = schema.validate(req.body, {abortEarly:false}) 
+       
+        if (!validation.error) {
+            next() // todo ok
+            
+         }else {
+            res.json({succes: false, errores: validation.error})
+           
+    }
+}
+    
+
+
+}
+module.exports = validator
