37e0ad42309f7211f1957dc7bab3b818ac1fc901
lucia <pereyra.lucia@gmail.com>
2021-02-13 22:18:53 -0300

    pull modif server

--- server.js
+++ server.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const express = require("express") 
-require("dotenv").config()
-const cors = require("cors") 
-const router = require("./routes/index")
-require("./config/database")
-
-const app = express()
-
-
-
-app.use(cors()) 
-app.use(express.json()) 
-
-app.use("/api", router)
-
-
-
-app.listen(4000, () => console.log("App listening on port 4000")) 
-=======
 const express = require('express')
 const cors = require('cors')
 
@@ -33,4 +13,3 @@ app.use(express.json())
 app.use('/api', router)
 
 app.listen(4000, ()=> console.log('app listening in the port 4000'))
->>>>>>> e2b47d631f704ea967f40028a8807d203bf18e7a
