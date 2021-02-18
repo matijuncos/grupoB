b66d56c441b74ff73c50fc73ff18ba37bcb28fdf
lucia <pereyra.lucia@gmail.com>
2021-02-18 16:29:01 -0300

    agrego texto en componente slider, renderiza en slide

--- frontend/src/components/Slide.jsx
+++ frontend/src/components/Slide.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Slide = ({ slide }) => {
   return (
     <div style={{ backgroundImage: `url(${slide.src})` }} className="slideImg">
-      <h2>¿Qué servicio para tu casa necesitas hoy?</h2>
+      <h2>{slide.txt}</h2>
     </div>
   )
 }
 
 export default Slide
+  
--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -5,19 +5,26 @@ import Slider from "react-slick";
 import Slide from './Slide';
 
 const pics = [{
-  src: '/assets/sliderImg/slide1.jpg'
+  src: '/assets/sliderImg/slide1.jpg',
+  txt: "¿Qué servicio para tu casa necesitas hoy?"
 }, {
-  src: '/assets/sliderImg/slide2.jpg'
+  src: '/assets/sliderImg/slide2.jpg',
+  txt: "Te ofrecemos el mejor servicio "
 }, {
-  src: '/assets/sliderImg/slide3.jpg'
+  src: '/assets/sliderImg/slide3.jpg',
+  txt: "Tenemos a los mejores profesionales para vos"
 }, {
-  src: '/assets/sliderImg/slide4.jpg'
+  src: '/assets/sliderImg/slide4.jpg',
+  txt: "Contratá con confianza y olvidate del chanta"
 }, {
-  src: '/assets/sliderImg/slide5.jpg'
+  src: '/assets/sliderImg/slide5.jpg',
+  txt: "Comprendemos tu urgencia, pensamos en vos "
 }, {
-  src: '/assets/sliderImg/slide6.jpg'
+  src: '/assets/sliderImg/slide6.jpg',
+  txt: "Podes elegir entre los mejores "
 }, {
-  src: '/assets/sliderImg/slide7.jpg'
+  src: '/assets/sliderImg/slide7.jpg',
+  txt: "La decisión es tuya, hacé click y relajate"
 }
 ]
 
