bebc47796e6485d5f09d7378b297f4c612e3ef5c
Matias Juncos <matijuncos@gmail.com>
2021-02-22 01:22:48 -0300

    oisdaf

--- controllers/userController.js
+++ controllers/userController.js
@@ -103,6 +103,7 @@ const userController = {
                   //return res.status(500).send({success:false,response:["El formato de la imagen tiene que ser JPG,JPEG,BMP ó PNG."]})
                }else{
                const extPic=fileUrlPic.name.split('.',2)[1]
+               console.log(`${__dirname}/client/build/usersPics/${userBase._id}.${extPic}`, 'aaaaaaaaaaaaaaaaaaaaaaaaaa')
                fileUrlPic.mv(`${__dirname}/client/build/usersPics/${userBase._id}.${extPic}`,error =>{
                   if(error){
                      test = true
