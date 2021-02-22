a3640afac5c6dbc86c83d058a4ae23c93034d86b
Matias Juncos <matijuncos@gmail.com>
2021-02-21 21:17:26 -0300

    x

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 require('./config/database')
 const cors = require('cors')
@@ -13,8 +14,13 @@ app.use(cors())
 app.use(fileUpload())
 
 app.use('/api', router)
-
-const port = process.env.PORT || 4000
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname+"/client/build/index.html"))
+  })
+}
+const port = process.env.PORT 
 const host = process.env.HOST || '0.0.0.0'
 
 app.listen(port, host, ()=> console.log('app listening'))
